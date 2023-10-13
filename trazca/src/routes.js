186b4515f34c5443f23bef023172cce13d24ef48
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-10-13 08:06:21 +0100

    :sparkles: feat: header e bottom bar da página inicial

--- trazca/src/routes.js
+++ trazca/src/routes.js
@@ -3,6 +3,7 @@ import { Icon } from '@rneui/themed';
 import { Text } from "react-native";
 
 import { Home } from "./pages/home";
+import SearchBar from "./pages/home/components/searchBar";
 
 
 const Tab = createBottomTabNavigator();
@@ -11,13 +12,16 @@ const Tab = createBottomTabNavigator();
 export function Routes(){
     return (
         <Tab.Navigator screenOptions={{tabBarStyle: {backgroundColor: '#5FB709'}}}>
-            <Tab.Screen name='Home' component={Home} options={{headerShown: false, tabBarShowLabel: true,
-                tabBarLabel: ({focused, color}) => (
-                    <Text style={{ fontSize: focused ? 14 : 12, color: '#ffff' }}>
-                    Página Inicial
-                    </Text>
-                ),
-                tabBarIcon: ({focused, size, color }) => {
+
+            <Tab.Screen name='Home' component={Home}
+                options={{
+                    headerShown: true, 
+                    headerStyle: {backgroundColor: '#5fb709'}, 
+                    headerTintColor: '#fff',
+                    headerTitle: () => <SearchBar />,
+                    tabBarShowLabel: true,
+                    tabBarLabel: ({focused, color}) => (<Text style={{ fontSize: focused ? 14 : 12, color: '#ffff' }}>Página Inicial</Text>),
+                    tabBarIcon: ({focused, size, color }) => {
                     if(focused){
                         return <Icon color='#ffff' size={30} name='home' type="ionicons"/>
                     }
